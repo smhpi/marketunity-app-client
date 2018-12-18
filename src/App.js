8b5f817697ab7fd3accc8a17c33d425e118ad142
Hosen Poursaeid <poursaeid@outlook.com>
2018-12-18 09:14:59 -0500

    session login & logout

--- src/App.js
+++ src/App.js
@@ -1,40 +1,84 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav, NavItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import Routes from "./Routes";
+import { Auth } from "aws-amplify";
 import logo from "./logo.png";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isAuthenticated: false,
+      isAuthenticating: true
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      await Auth.currentSession();
+      this.userHasAuthenticated(true);
+    } catch (e) {
+      if (e !== "No current user") {
+        alert(e);
+      }
+    }
+
+    this.setState({ isAuthenticating: false });
+  }
+
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  };
+
+  handleLogout = async event => {
+    await Auth.signOut();
+    this.userHasAuthenticated(false);
+  };
+
   render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
     return (
-      <div className="App">
-        <div className="container">
-          <Navbar fluid collapseOnSelect>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <Link to="/" style={{ paddingTop: 0 }}>
-                  {" "}
-                  <img src={logo} alt="MarketUnity" />
-                </Link>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-            </Navbar.Header>
-            <Navbar.Collapse>
-              <Nav pullRight>
-                <LinkContainer to="/signup">
-                  <NavItem>Signup</NavItem>
-                </LinkContainer>
-                <LinkContainer to="/login">
-                  <NavItem>Login</NavItem>
-                </LinkContainer>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-          <Routes />
+      !this.state.isAuthenticating && (
+        <div className="App">
+          <div className="container">
+            <Navbar fluid collapseOnSelect>
+              <Navbar.Header>
+                <Navbar.Brand>
+                  <Link to="/" style={{ paddingTop: 0 }}>
+                    {" "}
+                    <img src={logo} alt="MarketUnity" />
+                  </Link>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+              </Navbar.Header>
+              <Navbar.Collapse>
+                <Nav pullRight>
+                  {this.state.isAuthenticated ? (
+                    <NavItem onClick={this.handleLogout}>Logout</NavItem>
+                  ) : (
+                    <Fragment>
+                      <LinkContainer to="/signup">
+                        <NavItem>Signup</NavItem>
+                      </LinkContainer>
+                      <LinkContainer to="/login">
+                        <NavItem>Login</NavItem>
+                      </LinkContainer>
+                    </Fragment>
+                  )}
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+            <Routes childProps={childProps} />
+          </div>
         </div>
-      </div>
+      )
     );
   }
 }
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
-import { Image } from "react-bootstrap";
+import { Image, Grid, Row, Col } from "react-bootstrap";
 import "./Home.css";
-import muhome from "../images/muhome.png";
+
+import mlogos from "../images/mlogos.png";
 
 class Home extends Component {
   render() {
     return (
       <div className="jumbotron">
         <div className="lander">
-          <h1>MarketUnity</h1>
-          <p>MarketPlaces Integration Portal</p>
-          <div>
-            <Image src={muhome} responsive />
-          </div>
+          <Grid>
+            <Row>
+              <Col md={8}>
+                <h1>MarketUnity</h1>
+                <p>MarketPlaces Integration Portal</p>
+                <div>
+                  <Image src={mlogos} responsive />
+                </div>
+              </Col>
+              <Col md={4}>
+                <h3>Welcome to app</h3>
+              </Col>
+            </Row>
+          </Grid>
         </div>
       </div>
     );
