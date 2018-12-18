0d0086c71c37f3f6c0e94f6cb4a9b2e81c58d181
Hosen Poursaeid <poursaeid@outlook.com>
2018-12-17 19:34:27 -0500

    config AWS

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "marketunity-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://jd7lsrmzi1.execute-api.us-east-2.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_OuE5GKvFq",
+    APP_CLIENT_ID: "62emokta45rbff9bffpf2t8bs5",
+    IDENTITY_POOL_ID: "us-east-2:e808ea35-2235-486c-a282-4de79da83f8b"
+  }
+};
